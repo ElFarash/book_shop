03a6939db1345a12b5b22b3fadb2c1f8da024138
Mohammad Naser <30439618+ElFarash@users.noreply.github.com>
2019-08-01 12:43:11 +0200

    database updated

--- sql/books.sql
+++ sql/books.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 30, 2019 at 06:26 PM
+-- Generation Time: Aug 01, 2019 at 12:41 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.6
 
@@ -40,7 +40,8 @@ CREATE TABLE `authors` (
 
 INSERT INTO `authors` (`id`, `name`, `description`) VALUES
 (1, 'Mollie Hemingway', 'Mollie Ziegler Hemingway is an American conservative columnist and political commentator. She is a senior editor at the online magazine The Federalist and a contributor for Fox News.'),
-(2, 'Tara Westover', 'Tara Westover is an American memoirist, essayist and historian. Her memoir Educated debuted at #1 on the New York Times bestseller list and was a finalist for a number of national awards, including the LA Times Book Prize, PEN America\'s Jean Stein Book Aw');
+(2, 'Tara Westover', 'Tara Westover is an American memoirist, essayist and historian. Her memoir Educated debuted at #1 on the New York Times bestseller list and was a finalist for a number of national awards, including the LA Times Book Prize, PEN America\'s Jean Stein Book Aw'),
+(3, 'Mark Levin', 'Mark Reed Levin is an American lawyer, author, and radio personality. He is the host of syndicated radio show The Mark Levin Show, as well as Life, Liberty & Levin on Fox News. Levin worked in the administration of President Ronald Reagan and was a chief ');
 
 -- --------------------------------------------------------
 
@@ -54,16 +55,27 @@ CREATE TABLE `books` (
   `rate` int(11) DEFAULT NULL,
   `author_id` int(10) DEFAULT NULL,
   `published_at` date DEFAULT NULL,
-  `image_path` varchar(255) DEFAULT NULL
+  `image_path` varchar(255) DEFAULT NULL,
+  `author_name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`id`, `title`, `rate`, `author_id`, `published_at`, `image_path`) VALUES
-(1, 'Educated: A Memoir', 8, 2, '2019-02-10', 'https://images-na.ssl-images-amazon.com/images/I/81XR45UdqkL.jpg'),
-(2, 'Justice on Trial: The Kavanaugh Confirmation and the Future of the Supreme Court', 9, 1, '2019-03-11', 'https://images-na.ssl-images-amazon.com/images/I/51tQHDThsTL._SX329_BO1,204,203,200_.jpg');
+INSERT INTO `books` (`id`, `title`, `rate`, `author_id`, `published_at`, `image_path`, `author_name`) VALUES
+(1, 'Educated: A Memoir', 8, 2, '2019-02-10', 'https://images-na.ssl-images-amazon.com/images/I/81XR45UdqkL.jpg', 'Tara Westover'),
+(2, 'Justice on Trial: The Kavanaugh Confirmation and the Future of the Supreme Court', 9, 1, '2019-03-11', 'https://images-na.ssl-images-amazon.com/images/I/51tQHDThsTL._SX329_BO1,204,203,200_.jpg', 'Mollie Hemingway'),
+(3, 'Unfreedom of the Press ', 9, 3, '2019-05-21', 'https://images-na.ssl-images-amazon.com/images/I/81Scw9HLl5L.jpg', 'Mark Levin\r\n'),
+(4, 'Witch Hunt', 8, 4, '2019-08-08', 'https://i.harperapps.com/covers/9780062960092/x510.jpg', 'gregg jarrett'),
+(5, 'Radicals, Resistance, and Revenge', 9, 5, '2019-09-27', 'https://images-na.ssl-images-amazon.com/images/I/51lXCepytGL._SX329_BO1,204,203,200_.jpg', 'Jeanine Pirro'),
+(6, 'Liars, Leakers, and Liberals', 3, 5, '2019-07-17', 'https://images-na.ssl-images-amazon.com/images/I/81UDmZOb1wL.jpg', 'Jeanine Pirro'),
+(7, 'Maybe You Should Talk to Someone', 4, 6, '2019-04-02', 'https://images-na.ssl-images-amazon.com/images/I/51OBv43TthL._SX329_BO1,204,203,200_.jpg', 'Lori Gottlieb'),
+(8, 'Trump vs. the Media ', 7, 1, '2017-04-25', 'https://images-na.ssl-images-amazon.com/images/I/51EfDxAyoJL._SX337_BO1,204,203,200_.jpg', 'Mollie Hemingway'),
+(9, 'Marry Him', 8, 6, '2011-02-01', 'https://images-na.ssl-images-amazon.com/images/I/41y2kiw0IUL._SX331_BO1,204,203,200_.jpg', 'Lori Gottlieb'),
+(10, 'Stick Figure', 10, 6, '2017-11-17', 'https://images-na.ssl-images-amazon.com/images/I/519S9wbpPIL._AC_SY400_.jpg', 'Lori Gottlieb'),
+(11, 'Maybe You Should Talk to Someone - life from both sides', 10, 6, '2019-02-02', 'https://images-na.ssl-images-amazon.com/images/I/41pg5NFijeL._SX325_BO1,204,203,200_.jpg', 'Lori Gottlieb'),
+(12, 'A Little Bit Country', 10, 7, '2019-05-21', 'https://images-na.ssl-images-amazon.com/images/I/51QnrWCM1HL.SX316.SY316.jpg', 'Debbie Macomber');
 
 -- --------------------------------------------------------
 
@@ -147,26 +159,16 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `mobi
 (39, 'ahmed', 'naser', 'ahmed.elfarash@gmail.com', '123456789', '01151234879', '6ae9c7ce606058249b69e38813ab5fbd'),
 (53, 'mohamoud', 'ahmed', 'mhmoudahmed@gmail.com', '123456789', '01224866972', '7ca9129f4b3b812fd681c38ce6b27936'),
 (54, 'ahmedd', 'fathy', 'ahmed.fathi74@gmail.com', '123456789', '01143177934', 'ec4089d86e63fa1a549393cad5e129db'),
-(74, 'mohamoud', 'ahmed', 'mhmoudahmed18@gmail.com', '1123456789', '01224866972', '0b34a16bce0d4b1b5af7730b6c889ca1'),
-(75, 'mohamoud', 'ahmed', 'mhmoudahmeweed@gmail.com', '123456789', '01224866972', '6951e6d69171761784310fdd3ca063f0'),
-(76, 'mohamoud', 'ahmed', 'mhmoudahmed12@gmail.com', '123456789', '01224866972', '9179d2dd73b777974fefd4a4e09ffb16'),
-(77, 'dwqwdq', 'qwdqwqd', 'ahmedfawaz@hotmail.co.uk', 'ahmedessam', '01100200125', '2160860d10b45a13448449bf77e72160'),
-(78, 'ahmed', 'essam', 'ahmedessam@hotmail.com', 'ahmed', '01100600297', '81d6a105f5f0059d98f7671a846d5038'),
-(79, 'ahmed', 'essam', 'ahmedessamm@hotmi.co', '12345', '01100600297', '33e5f949e208ca3da5144ea52dbf2a50'),
-(80, 'ahmed', 'essam', 'ahmedessamm@hotmi.com', '123456', '01100600297', '6aae116ae7e492402572fde7fab40ee0'),
-(81, 'ahmed', 'essam', 'ahmedfawazz@hotmail.co.uk', 'ahmed', '01100500123', '1d202bd4c0af678c2ae1e189c20b2edc'),
-(82, 'ahmed', 'essam', 'ahmedessam1996@gmail.com', 'ahmedfawaz', '01100600297', '78d42af132feb4850f2b041c69cc37eb'),
-(84, 'ahmed', 'fathy', 'mohammad.elfarashwewed@gmail.com', '123456789', '01151234879', '4420a89dd4eb27e62495ed759bc24806'),
-(86, 'bbk', 'j bkj', 'akjsa@gmail.com', '123456', '01221548763', '31a39ecfad2ef8d3334c8955285af9b9'),
-(87, 'iolnekf', 'ek', 'dsf@gmail.com', '654321', '01221548763', '918441b2ebe32b005a5de8b65a5d8ee7'),
-(88, 'kdsjnkj', 'iug', 'jhui@ff.com', 'asdd', '01221548763', '19fd26ad43751ffc824c175a9a611504'),
-(89, 'dfg', 'sdcfb', 'asd@dd.com', 'asdf', '01221548763', 'c471a530f8d5cd5e74e7e73d4c54c06b'),
-(90, 'ilknlk', 'jb', 'hkn@k.com', '123654', '01221548763', '32192b4054c40db8bc8ad758e71538a1'),
-(91, 'oji', 'ijn', 'kjn@we.com', '654321', '01221548763', 'a2e5a6cd03c9e052e81c36a5a5ddd9c8'),
-(92, 'bbk', 'j bkj', 'akjsja@gmail.co', 'zxcbnm', '01221548763', 'b93cf0a550c2a9d7dd8bf5e9a2e18c6d'),
-(93, 'ahmed', 'naser', 'mohammad.elfaras4h@gmail.com', '12345', '01151234879', '0f697bb9bbf9a25a12373ea9f067ca68'),
-(94, 'bbk', 'ek', 'akfjsa@gmail.com', 'zxcvbnm', '01221548763', 'abc44c00973b908a3776945afde74d33'),
-(95, 'ahmed', 'naser', 'ahmed_naser@gmail.com', '123456789', '01151234879', 'ef789f3bf5b357fde016732a1a71072f');
+(95, 'ahmed', 'naser', 'ahmed_naser@gmail.com', '123456789', '01151234879', 'ef789f3bf5b357fde016732a1a71072f'),
+(96, 'aa', 'aa', 'dsadssa@hotmail.com', '123456789', '01100600239', 'e51dbac89a92afb599975869278d9c09'),
+(97, 'dads', 'asdsaas', 'ddsa@hotmail.com', '1234567', '01100600732', '21f5c8e9b3fc1489717633b60cab5478'),
+(98, 'ahmed', 'fathy', 'af7200097@gmail.com', '123456789', '01143177394', '80998b7c9e11530a963302110102e0b6'),
+(99, 'ahmed', 'essam', 'ahmedessam@hotmail.com', 'ahmed', '01100600297', 'f256add92510bc11de344f6bab305c36'),
+(100, 'ahmeed', 'esssam', 'hhhhsdd@hotmail.com', 'ahmed', '01100200123', '1513f8b9b25e6b8d27c69300b3a8c69a'),
+(101, 'ahmedd', 'essamm', 'ahmedessamm@hotmail.com', '123456789', '01100600297', '4f9b3538b6fff727b28513820c7055d3'),
+(102, 'mohamoud', 'ahmed', 'mhmoudahmed188@gmail.com', '1234', '01224866972', 'a2cb5855f2947fa01659532aa6148b21'),
+(103, 'ahmed', 'naser', 'mohammad.elfaras4h@gmail.com', '12345', '01151234879', '35caf0c4e636efaeb96965d6eeb57e1c'),
+(104, 'mohamoud', 'fathy', 'mhmoudahmed1888@gmail.com', 'qwerty', '01224866972', '440a1fe2eb80c36a3c2ff2f97323bcce');
 
 --
 -- Indexes for dumped tables
@@ -216,13 +218,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=230;
 
 --
 -- AUTO_INCREMENT for table `genres`
@@ -246,7 +248,7 @@ ALTER TABLE `user's books`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=105;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
