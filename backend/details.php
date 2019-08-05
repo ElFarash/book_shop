9764ad74afb9977fb67be18676584f9ed83a41ef
Mohammad Naser <30439618+ElFarash@users.noreply.github.com>
2019-08-05 18:37:04 +0200

    url added

--- backend/details.php
+++ backend/details.php
@@ -11,7 +11,7 @@ if(isset($_GET['id'])){
   	echo json_encode($response);
 }
 
-$query = "SELECT books.id, books.image_path , books.title , books.rate  , authors.image as author_image , authors.name as author_name, authors.description  as author_bio , books.description as book_description 
+$query = "SELECT books.id , books.url , books.image_path , books.title , books.rate , authors.image as author_image , authors.name as author_name, authors.description  as author_bio , books.description as book_description 
 FROM authors , books , book_gallery
 WHERE  books.id = $book_id && authors.id = books.author_id && books.id = book_gallery.book_id
 ORDER by books.id";
@@ -19,14 +19,20 @@ ORDER by books.id";
 $result = $conn->query($query);
 $books_authors_details = $result->fetch_all(MYSQLI_ASSOC);
 
+$headers = apache_request_headers();
+$token = isset($headers['Authorization']) ?: '';
+
 $images = [];
+$book_info = [];
 foreach ($books_authors_details as $book) {
 	array_push($images, $book['image_path']);
 }
-
-$book_info = [];
 array_push($book_info, $books_authors_details[0]);
 unset($book_info[0]['image_path']);
+
+if (!$token) {
+	$book_info[0]['url'] = "";
+}
 
 
 $response = ['status' => 1 , 'message' => "Book's Info Loaded successfully!" ,'book_info' => $book_info[0] , 'book_images' => $images];
