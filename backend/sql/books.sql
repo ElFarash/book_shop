5c347d11bfec7d4043ef196108a79ff5fe99a778
Mohammad Naser <30439618+ElFarash@users.noreply.github.com>
2019-08-05 16:15:05 +0200

    database updated

--- backend/sql/books.sql
+++ backend/sql/books.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2019 at 04:02 PM
+-- Generation Time: Aug 05, 2019 at 04:14 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.6
 
@@ -76,7 +76,7 @@ INSERT INTO `books` (`id`, `title`, `rate`, `author_id`, `published_at`, `image_
 (4, 'Witch Hunt', 8, 4, '2019-08-08', 'https://i.harperapps.com/covers/9780062960092/x510.jpg', 'gregg jarrett', 'A witch-hunt or a witch purge is a search for people who have been labelled \"witches\" or a search for evidence of witchcraft, and it often involves a moral panic or mass hysteria'),
 (5, 'Radicals, Resistance, and Revenge', 9, 5, '2019-09-27', 'https://images-na.ssl-images-amazon.com/images/I/51lXCepytGL._SX329_BO1,204,203,200_.jpg', 'Jeanine Pirro', 'Picking up where her #1 New York Times bestseller, Liars, Leakers and Liberals, left off, Judge Jeanine Pirro of Fox\'s Justice with Judge Jeanine exposes the latest chapter in the unfolding liberal attack on our most basic values'),
 (6, 'Liars, Leakers, and Liberals', 3, 5, '2019-07-17', 'https://images-na.ssl-images-amazon.com/images/I/81UDmZOb1wL.jpg', 'Jeanine Pirro', 'Liars, Leakers, and Liberals: The Case Against the Anti-Trump Conspiracy is a 2018 book authored by Jeanine Pirro, an American TV personality, former judge, prosecutor, district attorney and Republican politician in New York.'),
-(7, 'Maybe You Should Talk to Someone', 4, 6, '2019-04-02', 'dsfs', 'Lori Gottlieb', 'sdasd'),
+(7, 'Maybe You Should Talk to Someone', 4, 6, '2019-04-02', 'https://images-na.ssl-images-amazon.com/images/I/51OBv43TthL._SX329_BO1,204,203,200_.jpg', 'Lori Gottlieb', 'sdasd'),
 (8, 'Trump vs. the Media ', 7, 1, '2017-04-25', 'https://images-na.ssl-images-amazon.com/images/I/51EfDxAyoJL._SX337_BO1,204,203,200_.jpg', 'Mollie Hemingway', 'How bad is the problem of media bias? The answer can be summed up in a few words: President Donald J. Trump'),
 (9, 'Marry Him', 8, 6, '2011-02-01', 'https://images-na.ssl-images-amazon.com/images/I/41y2kiw0IUL._SX331_BO1,204,203,200_.jpg', 'Lori Gottlieb', 'An eye-opening, funny, painful, and always truthful in-depth examination of modern relationships and a wake-up call for single women about getting real about Mr. Right.'),
 (10, 'Stick Figure', 10, 6, '2017-11-17', 'https://images-na.ssl-images-amazon.com/images/I/519S9wbpPIL._AC_SY400_.jpg', 'Lori Gottlieb', 'Stick Figure is an American reggae and dub band founded in 2006 and based in Northern California'),
