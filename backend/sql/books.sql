82a922f2e30b0be112a9f5932083f5a2b19370b9
Mohammad Naser <30439618+ElFarash@users.noreply.github.com>
2019-08-25 16:59:56 +0200

    database update

--- backend/sql/books.sql
+++ backend/sql/books.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 25, 2019 at 04:51 PM
+-- Generation Time: Aug 25, 2019 at 04:59 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.6
 
